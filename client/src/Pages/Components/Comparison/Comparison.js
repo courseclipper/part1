8129b27ad5050c223d56c73394dcc25cadd17e85
Hammad Saeed <hammad.saeed776@gmail.com>
2024-03-18 17:40:50 +0500

    Fix review logo in comparison page

--- client/src/Pages/Components/Comparison/Comparison.js
+++ client/src/Pages/Components/Comparison/Comparison.js
@@ -163,22 +163,18 @@ const Comparison = () => {
               <div
                 className="rev-content"
                 onClick={() =>
-                  (window.location.href = review.AffiliatedLink
-                    ? review.AffiliatedLink
-                    : "#")
+                (window.location.href = review.AffiliatedLink
+                  ? review.AffiliatedLink
+                  : "#")
                 }
                 style={{ width: "100%", display: "flex", paddingTop: "20px" }}
                 key={`${index}-${review.courseName}`}
               >
                 <div style={{ minWidth: "74px", height: "100px" }}>
                   <img
-                    src={
-                      review.Logo
-                        ? review.Logo
-                        : "https://play-lh.googleusercontent.com/dsCkmJE2Fa8IjyXERAcwc5YeQ8_NvbZ4_OI8LgqyjILpXUfS5YhEcnAMajKPrZI-og"
-                    }
+                    src={`https://logo.clearbit.com/${review?.Logo}`}
                     alt=""
-                    style={{ width: "100%", height: "100%" }}
+                    style={{ width: "65px", height: "65px" }}
                   />
                 </div>
                 <div>
