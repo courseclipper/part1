d42cc4c57fed3205423a80e3c83d9d863accac90
Hammad Saeed <hammad.saeed776@gmail.com>
2024-04-01 05:27:21 +0500

    Add review modal to display individual review

--- client/src/Pages/Components/Reviews/Reviews.js
+++ client/src/Pages/Components/Reviews/Reviews.js
@@ -1,16 +1,17 @@
 import React, { useEffect, useState, useCallback } from "react";
 import "../Reviews/Reviews.css";
-import { useNavigate } from "react-router";
 import Rating from "@mui/material/Rating";
 import Navbar from "../../Navbar/Navbar";
 import api from "../../../api";
-import { Stack, TextField } from "@mui/material";
+import { Box, Modal, Stack, TextField, Tooltip } from "@mui/material";
 
 const Reviews = () => {
-  const navigate = useNavigate();
   const [filteredReviews, setFilteredReviews] = useState([]);
   const [enterCourseTitle, setEnterCourseTitle] = useState('');
   const [review, setReview] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [selectedReview, setSelectedReview] = useState();
+
   const fetchReviews = useCallback(async () => {
     try {
       const response = await api.get("/reviews");
@@ -22,9 +23,11 @@ const Reviews = () => {
       console.log(err);
     }
   }, []);
+
   useEffect(() => {
     fetchReviews();
   }, []);
+
   const calculateTimeDifference = (timestamp) => {
     const currentTime = new Date();
     const uploadTime = new Date(timestamp);
@@ -67,6 +70,15 @@ const Reviews = () => {
     }, 500);
   }
 
+  const handleClickReview = (review) => {
+    setSelectedReview(review)
+    setModalOpen(true)
+
+    if (review.AffiliatedLink) {
+      window.location.href = review.AffiliatedLink
+    }
+  }
+
   return (
     <>
       <Navbar />
@@ -88,11 +100,7 @@ const Reviews = () => {
             return (
               <div
                 className="rev-content"
-                onClick={() =>
-                (window.location.href = item.AffiliatedLink
-                  ? item.AffiliatedLink
-                  : "#")
-                }
+                onClick={() => handleClickReview(item)}
                 key={id}
               >
                 <div className="rev-icon-star">
@@ -128,7 +136,9 @@ const Reviews = () => {
                     maxHeight: "100%",
                   }}
                 >
-                  <p>Title: <strong>{item.courseName || 'N/A'}</strong></p>
+                  <Tooltip title={item.courseName}>
+                    <p>Title: <strong>{item.courseName || 'N/A'}</strong></p>
+                  </Tooltip>
                   <div className="rev-time">
                     {calculateTimeDifference(item.TimeofUpload)}
                   </div>
@@ -143,7 +153,9 @@ const Reviews = () => {
                     maxHeight: "100%",
                   }}
                 >
-                  <p>Description: <strong>{item.courseDescription}</strong></p>
+                  <Tooltip title={item.courseDescription}>
+                    <p>Description: <strong>{item.courseDescription}</strong></p>
+                  </Tooltip>
                   <div className="rev-time">
                     {calculateTimeDifference(item.TimeofUpload)}
                   </div>
@@ -152,6 +164,89 @@ const Reviews = () => {
             );
           })}
         </div>
+
+        <Modal
+          open={modalOpen}
+          onClose={() => setModalOpen(false)}
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}
+        >
+          <Box sx={{
+            backgroundColor: 'white',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderRadius: '32px',
+            padding: '32px'
+          }}>
+            <div
+              className="rev-content"
+              style={{ maxWidth: '98vw', width: 'fit-content' }}
+            >
+              <div className="rev-icon-star">
+                <img
+                  src={`https://logo.clearbit.com/${selectedReview?.Logo}`}
+                  alt=""
+                  style={{ width: "65px", height: "65px" }}
+                />
+                <Rating
+                  name="read-only"
+                  size="large"
+                  value={selectedReview?.Rating}
+                  readOnly
+                  style={{
+                    color: "green",
+                    position: "relative",
+                    bottom: "0.7rem",
+                  }}
+                />
+              </div>
+              <div className="rev-cust-cont1">
+                <span>{selectedReview?.username}</span>
+                <span>reviewed</span>
+                <span>{selectedReview?.platformName}</span>
+              </div>
+              <div
+                className="rev-content1"
+                style={{
+                  whiteSpace: "nowrap",
+                  overflow: "hidden",
+                  textOverflow: "ellipsis",
+                  maxWidth: "100%",
+                  maxHeight: "100%",
+                }}
+              >
+                <Tooltip title={selectedReview?.courseName}>
+                  <p>Title: <strong>{selectedReview?.courseName || 'N/A'}</strong></p>
+                </Tooltip>
+                <div className="rev-time">
+                  {calculateTimeDifference(selectedReview?.TimeofUpload)}
+                </div>
+              </div>
+              <div
+                className="rev-content1"
+                style={{
+                  whiteSpace: "nowrap",
+                  overflow: "hidden",
+                  textOverflow: "ellipsis",
+                  maxWidth: "100%",
+                  maxHeight: "100%",
+                }}
+              >
+                <Tooltip title={selectedReview?.courseDescription}>
+                  <p>Description: <strong>{selectedReview?.courseDescription}</strong></p>
+                </Tooltip>
+                <div className="rev-time">
+                  {calculateTimeDifference(selectedReview?.TimeofUpload)}
+                </div>
+              </div>
+            </div>
+          </Box>
+        </Modal>
       </div>
     </>
   );
